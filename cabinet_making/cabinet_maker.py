15b55eac7f5647a38472c9d92808067b8c7986eb
Marko Djordjic <marko.djordjic@outlook.com>
2025-01-01 10:36:45 +0100

    bugfix(plotting): Correction of plotting when there are multiple sections w/ varying number of doors per section.

--- cabinet_making/cabinet_maker.py
+++ cabinet_making/cabinet_maker.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from cabinet_making.constructions import FloorCabinet, WallCabinet, Cupboard
 from cabinet_making.measurements import CupboardElevation
 from cabinet_making.plots import CabinetPlotter
 
@@ -24,7 +25,8 @@ class CabinetMaker:
                  drawers: list[int] = None,
                  drawer_reference: int = 0,
                  drawer_front: list[int] = None,
-                 sections: list[int] = None,) -> None:
+                 sections: list[int] = None,
+                 doors_per_section: list[int] = None) -> None:
         self.cabinet_type = cabinet_type
         self.cabinet_name = cabinet_name
         self.orientation = orientation
@@ -37,6 +39,7 @@ class CabinetMaker:
         self.drawer_reference = drawer_reference
         self.shelves = shelves
         self.sections = sections
+        self.doors_per_section = doors_per_section
         self.height_inch = None
         self.depth_inch = None
         self.width_inch = None
@@ -58,6 +61,7 @@ class CabinetMaker:
         self.section_positions = None
         self.system_holes = None
         self.section_pairs_positions = None
+        self.measurements = None
         self.cabinet = None
         self.plotter = None
 
@@ -75,7 +79,6 @@ class CabinetMaker:
         self.cabinet.compute_elevation()
         self.cabinet.write_elevation()
 
-
     def _plotting(self):
         plot_file = Path(self.cabinet_name + '_section_and_elevation.pdf')
         self.plotter = CabinetPlotter(
@@ -89,12 +92,47 @@ class CabinetMaker:
             drawers=self.cabinet.get_drawers(),
             drawer_front=self.drawer_front,
             sections=self.sections,
+            doors_per_section=self.doors_per_section,
             section_pairs=self.cabinet.get_section_indications(),
             system_holes=self.cabinet.get_system_holes(),
         )
         self.plotter.plot_cabinet(compute_only=True, plot_file=plot_file)
 
+    def _compute_material(self):
+        match self.cabinet_type:
+            case 'floor':
+                measurements = FloorCabinet(
+                    height=self.height_mm,
+                    width=self.width_mm,
+                    depth=self.depth_mm,
+                    drawers=self.drawers,
+                    doors=0,
+                    sections=self.sections,
+                    doors_per_section=self.doors_per_section          
+                )
+            case 'wall':
+                measurements = WallCabinet(
+                    height=self.height_mm,
+                    width=self.width_mm,
+                    depth=self.depth_mm,
+                    doors=1,  # Doors need to be added to the CabinetMaker class.        
+                )
+            case 'cupboard':
+                measurements = Cupboard(
+                    back_tolerance=6,
+                    height=self.height_mm,
+                    width=self.width_mm,
+                    depth=self.depth_mm,
+                    front_sections=self.sections,
+                    drawers=self.drawer_front,
+                    h_dividers=self.dividers,
+                    doors_per_section=[1, 1]
+                )
 
+        material = measurements.compute_total_material()
+        self.measurements = material
+ 
     def make_cabinet(self):
+        self._compute_material()
         self._make_elevation()
         self._plotting()
